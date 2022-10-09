c7105a816dd1bab0525c04afa8e51974cb6fc6b8
Jeff Steele <steelem81@gmail.com>
2022-10-09 11:23:41 -0600

    Zookeeper 1 - intial

---
+++ java_oop/Zookeeper/src/animal/GorillaTest.java
@@ -0,0 +1,16 @@
+package animal;
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		Gorilla g1 = new Gorilla();
+		g1.throwSomething(3);
+		g1.displayEnergy();
+		g1.eatBananas(2);
+		g1.displayEnergy();
+		g1.climb(1);
+		g1.displayEnergy();
+
+	}
+
+}
