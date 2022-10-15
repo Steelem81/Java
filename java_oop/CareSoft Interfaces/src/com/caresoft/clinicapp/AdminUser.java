046406187d02edd8a4cb90cd38f1f7babe7bf90c
Jeff Steele <steelem81@gmail.com>
2022-10-15 12:50:33 -0600

    implemented interfaces

--- java_oop/CareSoft Interfaces/src/com/caresoft/clinicapp/AdminUser.java
+++ java_oop/CareSoft Interfaces/src/com/caresoft/clinicapp/AdminUser.java
@@ -1,5 +1,81 @@
 package com.caresoft.clinicapp;
 
-public class AdminUser extends User implements HIPAACompliantUser, HIPPACompliantAdmin {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
+    
+    // Inside class:
+    private String role;
+    private ArrayList<String> securityIncidents;
+    public AdminUser() {
+    	
+    }
+    
+    public AdminUser(int id, String role) {
+    	super.setId(id);
+    	this.setRole(role);
+    	this.securityIncidents = new ArrayList<String>();
+    }
+    
+    // TO DO: Implement a constructor that takes an ID and a role
+    // TO DO: Implement HIPAACompliantUser!
+    // TO DO: Implement HIPAACompliantAdmin!
+    
+    public void newIncident(String notes) {
+        String report = String.format(
+            "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n", 
+            new Date(), this.id, notes
+        );
+        securityIncidents.add(report);
+    }
+    
+    public void authIncident() {
+        String report = String.format(
+            "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
+            new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+        );
+        securityIncidents.add(report);
+    }
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public ArrayList<String> getSecurityIncidents() {
+		return securityIncidents;
+	}
+	public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+		this.securityIncidents = securityIncidents;
+	}
+
+	@Override
+	public ArrayList<String> reportSecurityIncidents() {
+		return this.getSecurityIncidents();
+	}
+
+	@Override
+	public boolean assignPin(int pin) {
+		boolean pinGt6 = false;
+		if (String.valueOf(pin).length() >= 6) {
+			pinGt6 = true;
+			this.pin = pin;
+		}
+		return pinGt6;
+	}
+
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		boolean confirmed = false;
+	
+		if (this.id == confirmedAuthID) {
+			confirmed = true;
+		}
+		this.authIncident();
+		return confirmed;
+	}
+    
+    // TO DO: Setters & Getters
 
 }
--- java_oop/CareSoft Interfaces/src/com/caresoft/clinicapp/Physician.java
+++ java_oop/CareSoft Interfaces/src/com/caresoft/clinicapp/Physician.java
@@ -1,5 +1,52 @@
 package com.caresoft.clinicapp;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Physician extends User implements HIPAACompliantUser {
+  
+    private ArrayList<String> patientNotes;
+	
+    public Physician() {
+    }
+    
+    public Physician(int id) {
+    	super.setId(id);
+    }
+    
+    public Physician(int id, int pin) {
+    	super.setId(id);
+    	this.setPin(pin);
+    }
+    // TO DO: Implement HIPAACompliantUser!
+	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+	@Override
+	public boolean assignPin(int pin) {
+		boolean pinGt4 = false;
+		if(String.valueOf(pin).length() >= 4) {
+			pinGt4 = true;
+			this.pin = pin;
+		}
+		return pinGt4;
+	}
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		boolean confirmed = false;
+		if (this.id == confirmedAuthID) {
+			confirmed = true;
+			
+		}
+		return confirmed;
+	}
+	
+    // TO DO: Setters & Getters
 
 }
